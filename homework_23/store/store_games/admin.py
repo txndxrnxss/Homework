57f18b9239d2c132002db8d10b06284bb79fa497
SofiaAleksandrova <sofia.gerdeva02@gmail.com>
2023-05-02 00:22:39 +0300

    Добавлены файлы.

--- homework_23/store/store_games/admin.py
+++ homework_23/store/store_games/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
+from .models import Game, Category
 
-from store_games.models import Category, Game
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    ...
+    fields = ('title', 'slug', 'description', 'is_active', 'game_amount')
 
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    ...
+    fields = ('name', 'slug', 'pub_date', 'release_date', 'price', 'category', 'description', 'is_active')
--- homework_23/store/store_games/urls.py
+++ homework_23/store/store_games/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),  
     path('home/<sort>', views.home_sort, name='sort'),  # сортировка по name или price
-    path('home/game/<game>', views.game_name, name='sort'),  
-    path('home/category/<category>', views.category_id, name='sort')
+    path('home/game/<game>', views.game_name, name='game'),  
+    path('home/category/<category>', views.category_id, name='category')
 ]
