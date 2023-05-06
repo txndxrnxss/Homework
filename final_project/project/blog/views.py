39be54672379c3709081c77b64efff48a9099172
SofiaAleksandrova <sofia.gerdeva02@gmail.com>
2023-05-06 23:39:57 +0300

    Начата верстка страниц, добавлен хеддер.

--- final_project/project/blog/views.py
+++ final_project/project/blog/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from blog.models import User
 
 def test(request):
-    form = 3
-    data = User.objects.filter(login = 'txndxrnxss').values('password')
-    print(data)
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html')
